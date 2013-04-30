21f6bd2cf2d85df84e11b013dca2e646a397461b
Dan Finnie <dan@danfinnie.com>
2013-04-30 19:55:20 -0400

    Basic import of Facebook API

--- script.js
+++ script.js
@@ -9,4 +9,33 @@ $(function() {
         links = $.map(ev.originalEvent.files, function(a) { return a.link; });
         events.trigger("imagesSelected", [links]);
     });
+
 });
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '371594599611866',
+      channelUrl : 'http://dropbox-facebook.localhost/channel.html',
+      status     : true,
+      xfbml      : true
+    });
+
+    FB.getLoginStatus(function(response) {
+        console.log("Facebook login status: " + response.status);
+        if (response.status === 'connected') {
+        // connected
+        } else if (response.status === 'not_authorized') {
+        // not_authorized
+        } else {
+        // not_logged_in
+        }
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
